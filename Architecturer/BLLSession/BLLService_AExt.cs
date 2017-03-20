84d366feb4ce7a6517408224f89227954956de9f
dnaiel <dog830228@gmail.com>
2017-03-20 18:13:18 +0800

    新增 readme內容

--- Architecturer/BLLSession/BLLService_AExt.cs
+++ Architecturer/BLLSession/BLLService_AExt.cs
@@ -1,149 +1,122 @@
-﻿
+﻿ 
 using Service_A;
 using IBLL;
 namespace ServiceRepository
-{
+{  
     /*
         資料倉儲要類別實體由這邊拿
-    */
-    public partial class BLLService_A : IBLLSession
+    */    
+	public partial class BLLService_A : IBLLSession
     {
-
-        private IActive_RecordBLL iActive_RecordBLL = null;
-        public IActive_RecordBLL IActive_RecordBLL
-        {
-            get
-            {
-                if (iActive_RecordBLL == null)
-                    iActive_RecordBLL = new Active_RecordBLL();
+        	
+        private IActive_RecordBLL iActive_RecordBLL=null;
+        public IActive_RecordBLL IActive_RecordBLL{
+           get{
+                if(iActive_RecordBLL==null)
+                    iActive_RecordBLL=new Active_RecordBLL();
                 return iActive_RecordBLL;
-            }
-            set
-            {
-                iActive_RecordBLL = value;
-            }
+              }
+           set{
+                iActive_RecordBLL=value;
+              }
         }
-
-        private IActive_Record_ItemsBLL iActive_Record_ItemsBLL = null;
-        public IActive_Record_ItemsBLL IActive_Record_ItemsBLL
-        {
-            get
-            {
-                if (iActive_Record_ItemsBLL == null)
-                    iActive_Record_ItemsBLL = new Active_Record_ItemsBLL();
+        	
+        private IActive_Record_ItemsBLL iActive_Record_ItemsBLL=null;
+        public IActive_Record_ItemsBLL IActive_Record_ItemsBLL{
+           get{
+                if(iActive_Record_ItemsBLL==null)
+                    iActive_Record_ItemsBLL=new Active_Record_ItemsBLL();
                 return iActive_Record_ItemsBLL;
-            }
-            set
-            {
-                iActive_Record_ItemsBLL = value;
-            }
+              }
+           set{
+                iActive_Record_ItemsBLL=value;
+              }
         }
-
-        private IActive_RuleBLL iActive_RuleBLL = null;
-        public IActive_RuleBLL IActive_RuleBLL
-        {
-            get
-            {
-                if (iActive_RuleBLL == null)
-                    iActive_RuleBLL = new Active_RuleBLL();
+        	
+        private IActive_RuleBLL iActive_RuleBLL=null;
+        public IActive_RuleBLL IActive_RuleBLL{
+           get{
+                if(iActive_RuleBLL==null)
+                    iActive_RuleBLL=new Active_RuleBLL();
                 return iActive_RuleBLL;
-            }
-            set
-            {
-                iActive_RuleBLL = value;
-            }
+              }
+           set{
+                iActive_RuleBLL=value;
+              }
         }
-
-        private IActive_TypeBLL iActive_TypeBLL = null;
-        public IActive_TypeBLL IActive_TypeBLL
-        {
-            get
-            {
-                if (iActive_TypeBLL == null)
-                    iActive_TypeBLL = new Active_TypeBLL();
+        	
+        private IActive_TypeBLL iActive_TypeBLL=null;
+        public IActive_TypeBLL IActive_TypeBLL{
+           get{
+                if(iActive_TypeBLL==null)
+                    iActive_TypeBLL=new Active_TypeBLL();
                 return iActive_TypeBLL;
-            }
-            set
-            {
-                iActive_TypeBLL = value;
-            }
+              }
+           set{
+                iActive_TypeBLL=value;
+              }
         }
-
-        private ILibrary_BookBLL iLibrary_BookBLL = null;
-        public ILibrary_BookBLL ILibrary_BookBLL
-        {
-            get
-            {
-                if (iLibrary_BookBLL == null)
-                    iLibrary_BookBLL = new Library_BookBLL();
+        	
+        private ILibrary_BookBLL iLibrary_BookBLL=null;
+        public ILibrary_BookBLL ILibrary_BookBLL{
+           get{
+                if(iLibrary_BookBLL==null)
+                    iLibrary_BookBLL=new Library_BookBLL();
                 return iLibrary_BookBLL;
-            }
-            set
-            {
-                iLibrary_BookBLL = value;
-            }
+              }
+           set{
+                iLibrary_BookBLL=value;
+              }
         }
-
-        private ILibrary_BookImgaeBLL iLibrary_BookImgaeBLL = null;
-        public ILibrary_BookImgaeBLL ILibrary_BookImgaeBLL
-        {
-            get
-            {
-                if (iLibrary_BookImgaeBLL == null)
-                    iLibrary_BookImgaeBLL = new Library_BookImgaeBLL();
+        	
+        private ILibrary_BookImgaeBLL iLibrary_BookImgaeBLL=null;
+        public ILibrary_BookImgaeBLL ILibrary_BookImgaeBLL{
+           get{
+                if(iLibrary_BookImgaeBLL==null)
+                    iLibrary_BookImgaeBLL=new Library_BookImgaeBLL();
                 return iLibrary_BookImgaeBLL;
-            }
-            set
-            {
-                iLibrary_BookImgaeBLL = value;
-            }
+              }
+           set{
+                iLibrary_BookImgaeBLL=value;
+              }
         }
-
-        private ILogin_LogBLL iLogin_LogBLL = null;
-        public ILogin_LogBLL ILogin_LogBLL
-        {
-            get
-            {
-                if (iLogin_LogBLL == null)
-                    iLogin_LogBLL = new Login_LogBLL();
+        	
+        private ILogin_LogBLL iLogin_LogBLL=null;
+        public ILogin_LogBLL ILogin_LogBLL{
+           get{
+                if(iLogin_LogBLL==null)
+                    iLogin_LogBLL=new Login_LogBLL();
                 return iLogin_LogBLL;
-            }
-            set
-            {
-                iLogin_LogBLL = value;
-            }
+              }
+           set{
+                iLogin_LogBLL=value;
+              }
         }
-
-        private ILogin_UserBLL iLogin_UserBLL = null;
-        public ILogin_UserBLL ILogin_UserBLL
-        {
-            get
-            {
-                if (iLogin_UserBLL == null)
-                    iLogin_UserBLL = new Login_UserBLL();
+        	
+        private ILogin_UserBLL iLogin_UserBLL=null;
+        public ILogin_UserBLL ILogin_UserBLL{
+           get{
+                if(iLogin_UserBLL==null)
+                    iLogin_UserBLL=new Login_UserBLL();
                 return iLogin_UserBLL;
-            }
-            set
-            {
-                iLogin_UserBLL = value;
-            }
+              }
+           set{
+                iLogin_UserBLL=value;
+              }
         }
-
-        private IparameterBLL iparameterBLL = null;
-        public IparameterBLL IparameterBLL
-        {
-            get
-            {
-                if (iparameterBLL == null)
-                    iparameterBLL = new parameterBLL();
+        	
+        private IparameterBLL iparameterBLL=null;
+        public IparameterBLL IparameterBLL{
+           get{
+                if(iparameterBLL==null)
+                    iparameterBLL=new parameterBLL();
                 return iparameterBLL;
-            }
-            set
-            {
-                iparameterBLL = value;
-            }
+              }
+           set{
+                iparameterBLL=value;
+              }
         }
-
+        	
     }
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 ![logo](https://github.com/isdaniel/3-Tier-Architecture-T4-EF/blob/master/ArchitecturePic.png?raw=true)
+
 這是一個三層式架構+EF+T4模板
 可以依照需求來做彈性化的變化
+
+說明:
+	UI層:此層可以是Console or .Net webForm or .Net Mvc...
+================================================================
+	BLL層:
+		IBLL:宣告BLL層有哪些方法可以實現,給UI來呼叫
+			(1)IBaseBLL:簡單CURD方法給子類來實現
+			(2)IBLLFactory:一個介面給Repository來實現工廠，降低耦合
+			(3)IBLLSession:宣告屬性讓BLL子類實現，可用partial來獨立宣告行為
+		BLL:實現IBLL執行業務邏輯，可以有多個BLL
+			(1)BaseBLL:繼承IBaseBLL並實現CURD方法,並提供SetDal給繼承他的子類實現要用的Dal
+			(2)BLL:用t4模板繼承相對應名稱的介面並重載BaseBLL.SetDal方法，可用partial讓類別易擴充		
+		BLLRepository:創建BLL給外部來使用(類似實體工廠)
+			(1)BLLFactory:繼承IBLLFactory並實現裡面GetBLLSession方法,用抽象工廠方便日後更換邏輯模塊
+			(2)BLLService_A:繼承IBLLSession在此創建BLL類別
+================================================================
+	DAL層:
+		IDAL:宣告DAL層有哪些方法可以實現,給BLL來呼叫
+			(1)IBaseIDAL:簡單CURD方法給子類來實現
+			(2)IFactoryDal:一個介面給Repository來實現工廠，降低耦合
+			(3)IDALSession:宣告屬性讓DAL子類實現，可用partial來獨立宣告行為
+		DAL:實現IDAL執行業務邏輯，可以有多個DAL
+			(1)BaseDAL:繼承IBaseIDAL並實現CURD方法
+			(2)DAL:用t4模板繼承相對應名稱的介面
+		DALRepository:創建DAL給外部來使用(類似實體工廠)
+			(1)FactoryDal:繼承IFactoryDal並實現裡面GetDbSession方法,用抽象工廠方便日後更換讀取資料模塊
+================================================================	
+	Infrastructure:
+		Model:裡面放置模組
+		DI:製作容器
+		Common:在裡面宣告公用方法
+			
